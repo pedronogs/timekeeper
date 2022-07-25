a8f2653727d497bdb3c2715f8d97f3a12d0aadee
pedronogs <phnogs@hotmail.com>
2022-07-24 22:44:27 -0300

    feat: add MongoDB integration, fix tasks POST form and fix types

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,12 +1,13 @@
-from http.client import HTTPException
-from fastapi import FastAPI, UploadFile, HTTPException
-from fastapi.staticfiles import StaticFiles
-from fastapi.middleware.cors import CORSMiddleware
 import uuid
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from http.client import HTTPException
 from typing import List, Type
 
-from backend.src.models.Task import Task
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from backend.src.models.Task import TaskRequest, TaskResponse
+from fastapi import FastAPI, File, Form, HTTPException, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from pymongo import MongoClient
 
 app = FastAPI()
 scheduler = None
@@ -24,39 +25,61 @@ app.add_middleware(
 
 @app.on_event('startup')
 def init_data():
+    global mongo_client
+    mongo_client = MongoClient("mongodb://localhost:27017/")
+
     global scheduler
     scheduler = AsyncIOScheduler()
     scheduler.start()
 
 
 @app.get("/api/tasks")
-async def get_tasks() -> List[Task]:
-    tasks: List[Task] = []
+async def get_tasks() -> List[TaskResponse]:
+    tasks: List[TaskResponse] = []
     for job in scheduler.get_jobs():
         tasks.append(
-            Task(id=job.id,
-                 schedule=str(job.trigger),
-                 next_run_time=job.next_run_time))
+            TaskResponse(id=job.id,
+                         name=job.name,
+                         trigger=str(job.trigger),
+                         last_run_time=None,
+                         next_run_time=job.next_run_time))
 
     return tasks
 
 
 @app.post("/api/tasks")
-async def create_task(file: UploadFile):
+async def create_task(name: str = Form(...),
+                      trigger: str = Form(...),
+                      file: UploadFile = File(...)) -> TaskResponse:
+    task_data = TaskRequest(name=name, trigger=trigger)
+
     file_ext = file.filename.split(".")[-1]
-    if file.content_type != "application/octet-stream" or file_ext != "py":
+    print(file.content_type, file_ext)
+    if file.content_type != "text/x-python" or file_ext != "py":
         raise HTTPException(status_code=400, detail="Invalid file type.")
 
     content = await file.read()
 
     rname = str(uuid.uuid4())
-    with open(f"backend/tasks/{rname}.py", "wb") as f:
+    fpath = f"backend/tasks/{rname}.py"
+    with open(fpath, "wb") as f:
         f.write(content)
 
     task = lambda: exec(content)
-    scheduler.add_job(task, 'interval', seconds=10)
+    job = scheduler.add_job(task, 'interval', seconds=10)
 
-    return {"message": "Task created succesfully."}
+    mongo_client["timekeeper"]["tasks"].insert_one({
+        "id": job.id,
+        "name": task_data.name,
+        "trigger": task_data.trigger,
+        "filepath": fpath
+    })
+
+    return TaskResponse(id=job.id,
+                        name=task_data.name,
+                        trigger=task_data.trigger,
+                        last_run_time=None,
+                        next_run_time=job.next_run_time)
 
 
 app.mount("/",
--- backend/src/models/Task.py
+++ backend/src/models/Task.py
@@ -2,7 +2,14 @@ from datetime import datetime
 from pydantic import BaseModel
 
 
-class Task(BaseModel):
+class TaskRequest(BaseModel):
+    name: str
+    trigger: str
+
+
+class TaskResponse(BaseModel):
     id: str
-    schedule: str
+    name: str
+    trigger: str
+    last_run_time: datetime = None
     next_run_time: datetime
